8c4e430269cd55fcdb9fc8d5a7a2a4c72386a65e
jaronli3 <2002jaron@gmail.com>
2024-03-11 23:41:08 -0700

    progress

--- deepfake_audio.py
+++ deepfake_audio.py
@@ -12,7 +12,7 @@ def create_melspectrogram(file_name, mels, frames):
         #print(f)
         
         file_path = os.path.join("LA/LA/ASVspoof2019_LA_train/flac", f)
-        print(file_path)
+        #print(file_path)
         file_path = file_path.replace("\\", "/")
         #add full path name
         ndarray, rate = lb.load(file_path, sr = None)
@@ -46,19 +46,42 @@ def create_cnn():
     ...
 
 def main():
-    os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+    #os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
     training_dir_la = "LA/LA/ASVspoof2019_LA_train/flac"
+    validation_dir_la = "LA/LA/ASVspoof2019_LA_dev/flac"
     testing_dir_la = "LA/LA/ASVspoof2019_LA_eval/flac"
+    training_dir_pa = "PA/PA/ASVspoof2019_PA_train/flac"
+    validation_dir_pa = "PA/PA/ASVspoof2019_PA_eval/flac"
+    testing_dir_pa = "PA/PA/ASVspoof2019_PA_eval/flac"
     training_files_la = os.listdir(training_dir_la)
+    validation_files_la = os.listdir(validation_dir_la)
     testing_files_la = os.listdir(testing_dir_la)
-
-    x_train, y_train = create_melspectrogram(training_files_la, 128, 128)
-
-    print(x_train.shape)
-    print(y_train.shape)
-    print(x_train[0])
-    print(y_train[0])
+    training_files_pa = os.listdir(training_dir_pa)
+    validation_files_pa = os.listdir(validation_dir_pa)
+    testing_files_pa = os.listdir(testing_dir_pa)
     
+    counter = 0
+    counter1 = 0
+    counter2 = 0
+    counter3 = 0
+    for x in training_files_la:
+        counter += 1
+    for x in validation_files_la:
+        counter1 += 1
+    # for x in training_files_pa:
+    #     counter2 += 1
+    # for x in testing_files_pa:
+    #     counter3 += 1
+    print(counter)
+    print(counter1)
+    # print(counter2)
+    # print(counter3)
+
+    x_train_training, y_train_training = create_melspectrogram(training_files_la, 128, 128)
+    x_train_eval, y_train_eval = create_melspectrogram(validation_files_la, 128, 128)
+    x_train_testing, y_train_testing = create_melspectrogram(testing_files_la, 128, 128)
+    
+
 
 
 if __name__ == "__main__":
