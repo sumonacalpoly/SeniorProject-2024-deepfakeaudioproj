f84dbe2f98864df3d61df443284aa3c0154eaa53
jaronli3 <2002jaron@gmail.com>
2024-03-19 00:44:40 -0700

    switched to cross entropy

--- deepfake_audio.py
+++ deepfake_audio.py
@@ -2,6 +2,7 @@ import librosa as lb
 import numpy as np
 import tensorflow as tf
 import os
+import matplotlib.pyplot as plt
 #os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 #bonafide = 1, fake = 0
@@ -37,66 +38,6 @@ def create_melspectrogram(file_name, path, cm_path, mels, frames):
 
     return x_train, y_train
 
-# def create_melspectrogram(file_name_la, path_la, cm_path_la, file_name_pa, path_pa, cm_path_pa, mels, frames):
-#     x_train = []
-#     y_train = []
-#     for f in file_name_la:
-#         label = testing_real_or_fake(f, cm_path_la)
-#         if label == -1:
-#             continue
-#         # if label is None:
-#         #     print(f)
-#         #     print(cm_path)
-#         print(f)
-#         print(label)
-        
-#         file_path = os.path.join(path_la, f)
-        
-#         file_path = file_path.replace("\\", "/")
-#         print(file_path)
-#         #add full path name
-#         ndarray, rate = lb.load(file_path, sr = None)
-#         melspectrogram = lb.feature.melspectrogram(y=ndarray, sr=rate, n_mels = mels)
-#         if melspectrogram.shape[1] > frames:
-#             melspectrogram = melspectrogram[:, :frames]
-#         else:
-#             melspectrogram = np.pad(melspectrogram, ((0, 0), (0, frames - melspectrogram.shape[1])), mode='constant')
-#         x_train.append(melspectrogram)
-#         y_train.append(label)
-
-#     for f in file_name_pa:
-#         label = testing_real_or_fake(f, cm_path_pa)
-#         if label == -1:
-#             continue
-#         # if label is None:
-#         #     print(f)
-#         #     print(cm_path)
-#         print(f)
-#         print(label)
-        
-#         file_path = os.path.join(path_pa, f)
-        
-#         file_path = file_path.replace("\\", "/")
-#         print(file_path)
-#         #add full path name
-#         ndarray, rate = lb.load(file_path, sr = None)
-#         melspectrogram = lb.feature.melspectrogram(y=ndarray, sr=rate, n_mels = mels)
-#         if melspectrogram.shape[1] > frames:
-#             melspectrogram = melspectrogram[:, :frames]
-#         else:
-#             melspectrogram = np.pad(melspectrogram, ((0, 0), (0, frames - melspectrogram.shape[1])), mode='constant')
-#         x_train.append(melspectrogram)
-#         y_train.append(label)
-
-
-#     x_train = np.array(x_train)
-#     y_train = np.array(y_train)
-
-#     return x_train, y_train
-
-
-
-
 def testing_real_or_fake(fil, cm_path):
     #print(file)
     with open(cm_path, 'r') as f:
@@ -110,18 +51,23 @@ def testing_real_or_fake(fil, cm_path):
         return -1
                 
 def create_cnn(input_shape):
-    model = tf.keras.Sequential([tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape),
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape = input_shape),
+        tf.keras.layers.Conv2D(32, kernel_size = (3,3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size = (2,2)),
+        tf.keras.layers.Conv2D(64, kernel_size = (3,3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size = (2,2)),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(1, activation='sigmoid')])
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(.5), 
+        tf.keras.layers.Dense(2, activation="softmax")
+    ])
 
     model.compile(optimizer='adam',
-                loss='binary_crossentropy',
+                loss='categorical_crossentropy',
                 metrics=['accuracy'])
+    
+    #print(model.summary())
     return model
 
 def main():
@@ -144,37 +90,6 @@ def main():
     train_cm_path_pa = "PA/PA/ASVspoof2019_PA_cm_protocols/ASVspoof2019.PA.cm.train.trn.txt"
     val_cm_path_pa = "PA/PA/ASVspoof2019_PA_cm_protocols/ASVspoof2019.PA.cm.dev.trl.txt"
     test_cm_path_pa = "PA/PA/ASVspoof2019_PA_cm_protocols/ASVspoof2019.PA.cm.eval.trl.txt"
-
-    # if os.path.exists('x_train_training_all.npy') and os.path.exists('y_train_training_all.npy'):
-    #     print("here")
-    #     x_train_training_all = np.load('x_train_training_all.npy', allow_pickle=True)
-    #     y_train_training_all = np.load('y_train_training_all.npy', allow_pickle=True)
-    # else:
-    #     print("here1")
-    #     x_train_training_all, y_train_training_all = create_melspectrogram(training_files_la, training_dir_la, train_cm_path, training_files_pa, training_dir_pa, train_cm_path_pa, 128, 128)
-    #     np.save('x_train_training_all.npy', x_train_training_all)
-    #     np.save('y_train_training_all.npy', y_train_training_all)
-
-    # if os.path.exists('x_train_val_all.npy') and os.path.exists('y_train_val_all.npy'):
-    #     print("here2")
-    #     x_train_val_all = np.load('x_train_val_all.npy', allow_pickle=True)
-    #     y_train_val_all = np.load('y_train_val_all.npy', allow_pickle=True)
-    # else:
-    #     print("here3")
-    #     x_train_val_all, y_train_val_all = create_melspectrogram(validation_files_la, validation_dir_la, val_cm_path, validation_files_pa, validation_dir_pa, val_cm_path_pa, 128, 128)
-    #     np.save('x_train_val_all.npy', x_train_val_all)
-    #     np.save('y_train_val_all.npy', y_train_val_all)
-
-    # if os.path.exists('x_train_testing_all.npy') and os.path.exists('y_train_testing_all.npy'):
-    #     print("here4")
-    #     x_train_testing_all = np.load('x_train_testing_all.npy', allow_pickle=True)
-    #     y_train_testing_all = np.load('y_train_testing_all.npy', allow_pickle=True)
-    # else:
-    #     print("here5")
-    #     x_train_testing_all, y_train_testing_all = create_melspectrogram(testing_files_la, testing_dir_la, test_cm_path, testing_files_pa, testing_dir_pa, test_cm_path_pa, 128, 128)
-    #     np.save('x_train_testing_all.npy', x_train_testing_all)
-    #     np.save('y_train_testing_all.npy', y_train_testing_all)
-
 
     if os.path.exists('x_train_training.npy') and os.path.exists('y_train_training.npy'):
         x_train_training = np.load('x_train_training.npy', allow_pickle=True)
@@ -224,43 +139,21 @@ def main():
         np.save('x_train_val_pa.npy', x_train_val_pa)
         np.save('y_train_val_pa.npy', y_train_val_pa)
     
-    print(f"x_train_training: {x_train_training}")
-    print(f"y_train_training: {y_train_training}")
-    print(f"x_train_val: {x_train_val}")
-    print(f"y_train_val: {y_train_val}")
-    print(f"x_train_test: {x_train_testing}")
-    print(f"y_train_test: {y_train_testing}")
-
-    print(f"x_train_training_pa: {x_train_training_pa}")
-    print(f"y_train_training_pa: {y_train_training_pa}")
-    print(f"x_train_val_pa: {x_train_val_pa}")
-    print(f"y_train_val_pa: {y_train_val_pa}")
-    print(f"x_train_test_pa: {x_train_testing_pa}")
-    print(f"y_train_test_pa: {y_train_testing_pa}")
-        
-    # print(f"x_train_training_all: {x_train_training_all}")
-    # print(f"y_train_training_all: {y_train_training_all}")
-    # print(f"x_train_val_all: {x_train_val_all}")
-    # print(f"y_train_val_all: {y_train_val_all}")
-    # print(f"x_train_test_all: {x_train_testing_all}")
-    # print(f"y_train_test_all: {y_train_testing_all}")
-
-    # x_train_training_all = x_train_training_all.reshape(-1, 128, 128, 1)
-    # model = create_cnn(x_train_training_all.shape[1:])
-    # model.fit(x_train_training_all, y_train_training_all, epochs=10, batch_size=32, validation_data=(x_train_val_all, y_train_val_all))
-    # loss, accuracy = model.evaluate(x_train_testing_all, y_train_testing_all)
-    # print(accuracy)
+    y_train_training = tf.keras.utils.to_categorical(y_train_training, num_classes=2)
+    y_train_val = tf.keras.utils.to_categorical(y_train_val, num_classes=2)
+    y_train_testing = tf.keras.utils.to_categorical(y_train_testing, num_classes=2)
+    y_train_training_pa = tf.keras.utils.to_categorical(y_train_training_pa, num_classes=2)
+    y_train_val_pa = tf.keras.utils.to_categorical(y_train_val_pa, num_classes=2)
+    y_train_testing_pa = tf.keras.utils.to_categorical(y_train_testing_pa, num_classes=2)
 
     x_train_training = x_train_training.reshape(-1, 128, 128, 1)
     model = create_cnn(x_train_training.shape[1:])
-    model.fit(x_train_training, y_train_training, epochs=1, batch_size = 32, validation_data=(x_train_val, y_train_val))
+    model.fit(x_train_training, y_train_training, epochs=1, batch_size = 128, validation_data=(x_train_val, y_train_val))
     loss, accuracy = model.evaluate(x_train_testing, y_train_testing)
-    # print(accuracy)
-    # print(loss)
 
     x_train_training_pa = x_train_training_pa.reshape(-1, 128, 128, 1)
     model_pa = create_cnn(x_train_training_pa.shape[1:])
-    model_pa.fit(x_train_training_pa, y_train_training_pa, epochs=1, batch_size= 64, validation_data=(x_train_val, y_train_val))
+    model_pa.fit(x_train_training_pa, y_train_training_pa, epochs=1, batch_size= 64, validation_data=(x_train_val_pa, y_train_val_pa))
     loss_pa, accuracy_pa = model_pa.evaluate(x_train_testing_pa, y_train_testing_pa)
 
     print(accuracy)
